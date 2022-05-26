4fe31b6688f1796e9b821e32501b508c64668f46
Chummy <57281249+Jimmy01240397@users.noreply.github.com>
2022-05-27 03:06:56 +0800

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,10 +8,30 @@ function checkSignIn(req, res, next) {
   }
 }
 
-function addMeta(req, res, next) {
+async function addMeta(req, res, next) {
   if (req.session && req.session.user) {
+    header = {};
+    if (req.headers.cookie.includes('session'))
+    {
+      nowsession = req.headers.cookie.split('session=')[1];
+      if(nowsession.includes('; '))
+      {
+        nowsession = nowsession.split('; ')[0];
+      }
+      header = {
+        headers: {
+          'Cookie': 'session=' + nowsession + '; '
+        }
+      };
+    }
+    const body = {
+      username: req.session.user.studentId,
+    };
+    const result = await axios.post(vncproxyUrl + "/session", body, header);
+    res.cookie('session', result.headers['set-cookie'][0].split(';')[0].replace("session=",""));
     req.isSignIn = true;
   } else {
+    res.clearCookie('session')
     req.isSignIn = false;
   }
   next();
