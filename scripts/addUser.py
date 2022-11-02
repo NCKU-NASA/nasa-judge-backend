32c67d849c18f785d7ef6f0ec97f440a622b6a4f
chummy <jimmy012403976@gmail.com>
2022-11-03 03:32:19 +0800

    add env JUDGE_AT_LOCAL and fix addUser.py can check JUDGE_AT_LOCAL to use ssh control api or not

--- scripts/addUser.py
+++ scripts/addUser.py
@@ -9,6 +9,7 @@ username = os.getenv('DB_USER')
 password = os.getenv('DB_PASSWD')
 database = os.getenv('DB_NAME')
 judgeurl = os.getenv('JUDGE_URL')
+judgeatlocal = bool(os.getenv('JUDGE_AT_LOCAL'))
 table_name = 'user'
 
 if len(sys.argv) != 3:
@@ -44,7 +45,10 @@ db.close()
 try:
   os.mkdir(f'../files/{student_id}')
   os.system(f'chown -R www-data:www-data ../files/{student_id}')
-  os.system('ssh root@' + judgeurl.replace('http://','') + ' bash /etc/nasajudgeapi/addvpnuser.sh ' + student_id)
-#  os.system('bash /etc/nasajudgeapi/addvpnuser.sh ' + student_id)
+
+  if judgeatlocal:
+    os.system('bash /etc/nasajudgeapi/addvpnuser.sh ' + student_id)
+  else:
+    os.system('ssh root@' + judgeurl.replace('http://','') + ' bash /etc/nasajudgeapi/addvpnuser.sh ' + student_id)
 except FileExistsError:
   print('Directory already exists')
